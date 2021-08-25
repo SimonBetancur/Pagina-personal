16a9e9f2bca151d30852a1bcbe7a38961ec696e4
simonbeta <betancurbarrerasimon.personal@gmail.com>
2021-08-25 10:51:49 -0500

    Commit 1.1 (Primera parte semi-semi-terminada)

--- js/botones-perfil.js
+++ js/botones-perfil.js
@@ -1,6 +1,12 @@
 var correo = "betancurbarrerasimon@gmail.com";
 var telefono = "3023003612";
 
+//HTML
+
+onclick="funcioncorreo()"
+onclick="funciontelefono()"
+
+//HTML
 
 function funcioncorreo() {
     navigator.clipboard.writeText(correo);
